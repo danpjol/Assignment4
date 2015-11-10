7f2a5597a1ae5a196c6e1d248562449fda114714
danpjol <mbhv@iup.edu>
2015-11-10 17:26:18 -0500

    Create pageAlloc.h

---
+++ pageAlloc.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#ifndef PAGEALLOC_H_
+#define PAGEALLOC_H_
+typedef struct FREE{
+	struct FREE *next;
+} FREE;
+typedef struct PAGE{
+	int size;
+	int blocks;
+	void *availPtr;
+	struct PAGE *nextPg, *prevPg;
+	FREE *freePtr;
+} PAGE;
+
+void *pgalloc(int size);
+#endif
